330cfe1a7c1f5bea526f43d1aec82e9161a5f157
David Giovannini <dsjove@mac.com>
2025-11-22 09:29:56 -0600

    fix delete button

--- Sources/SBJKit/buttons/DeleteButton.swift
+++ Sources/SBJKit/buttons/DeleteButton.swift
@@ -3,25 +3,37 @@ import SwiftUI
 public struct DeleteButton : View {
 	let noun: String
 	let extra: String
-	let systemImage: String
+	let image: ImageName
+	let comfirm: Bool
 	let action: () -> Void
 	@State private var showingDeleteAlert = false
 
-	public init(_ noun: String, _ extra: String = "", systemImage: String? = nil, action: @escaping () -> Void, showingDeleteAlert: Bool = false) {
+	public init(
+			_ noun: String,
+			_ extra: String = "",
+			image: ImageName = .system("trash"),
+			comfirm: Bool = true,
+			action: @escaping () -> Void) {
 		self.noun = noun
 		self.extra = extra
-		self.systemImage = systemImage ?? "trash"
+		self.image = image
+		self.comfirm = comfirm
 		self.action = action
-		self.showingDeleteAlert = showingDeleteAlert
 	}
 
 	public var body: some View {
 		Button(role: .destructive) {
-			showingDeleteAlert = true
+			if comfirm {
+				showingDeleteAlert = true
+			}
+			else {
+				action()
+			}
 		} label: {
-			Label("Delete \(noun)", systemImage: systemImage)
+			Label("Delete \(noun)", image: image)
 		}
 		.labelStyle(.iconOnly)
+		.buttonStyle(.borderless)
 		.alert("Delete \(noun)", isPresented: $showingDeleteAlert, actions: {
 			Button("Delete", role: .destructive) {
 				action()
