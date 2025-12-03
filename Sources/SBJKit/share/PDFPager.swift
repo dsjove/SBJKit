4273cabc4b89a4961a24d07cf95d51a32a312317
David Giovannini <dsjove@mac.com>
2025-12-03 14:44:38 -0600

    add debug artifacts to PDFPager

--- Sources/SBJKit/share/PDFPager.swift
+++ Sources/SBJKit/share/PDFPager.swift
@@ -1,11 +1,18 @@
 import SwiftUI
+import UIKit
 
 public class PDFPager {
+	public var debug: Bool = false
 	//72 DPI
 	public let bounds = CGRect(x: 0, y: 0, width: 612, height: 792)
 	public let margin: Double = 36
-	public let maxY: Double
-	public let contentWidth: Double
+	public var contentRect: CGRect { .init(
+		x: margin, y: margin,
+		width: bounds.width - 2 * margin, height: bounds.height - 2 * margin)
+	}
+
+	public var maxY: Double { contentRect.maxY }
+	public var contentWidth: Double { contentRect.width }
 
 	public let renderer: UIGraphicsPDFRenderer
 
@@ -15,8 +22,6 @@ public class PDFPager {
 	public private(set) var y: Double
 
 	public init(_ name: String, _ bundle: String) {
-		self.maxY = bounds.height - margin
-		self.contentWidth = bounds.width - 2 * margin
 		self.y = margin
 
 		let format = UIGraphicsPDFRendererFormat()
@@ -40,6 +45,7 @@ public class PDFPager {
 		_header = header
 		ctx.beginPage()
 		page = 1
+		debugDrawFrames()
 		_header?(ctx.cgContext)
 	}
 
@@ -115,6 +121,7 @@ public class PDFPager {
 		if let url {
 			ctx?.cgContext.setURL(url as CFURL, for: drawRect)
 		}
+		debugDrawElement(drawRect)
 		let consumed = size.height + lineSpacing
 		if cursor == nil {
 			consume(consumed)
@@ -151,6 +158,7 @@ public class PDFPager {
 		}
 		let drawRect = CGRect(x: x, y: pos, width: size.width, height: size.height)
 		img.draw(in: drawRect)
+		debugDrawElement(drawRect)
 		let consumed = size.height + lineSpacing
 		if cursor == nil {
 			consume(consumed)
@@ -158,12 +166,24 @@ public class PDFPager {
 		return consumed
 	}
 
+	func debugDrawElement(_ drawRect: CGRect) {
+		if debug, let cg = ctx?.cgContext {
+			cg.saveGState()
+			cg.setStrokeColor(UIColor.green.cgColor)
+			cg.setLineWidth(0.5)
+			cg.stroke(drawRect)
+			cg.restoreGState()
+		}
+	}
+
 	@discardableResult
 	public func newPageIfNeeded(_ delta: Double) -> Bool {
 		if (y + delta) > maxY {
+			debugDrawPageBreak(delta)
 			ctx!.beginPage()
 			y = margin
 			page += 1
+			debugDrawFrames()
 			if let header = _header, let ctx = ctx?.cgContext {
 				header(ctx)
 			}
@@ -172,7 +192,58 @@ public class PDFPager {
 		return false
 	}
 
+	private func debugDrawPageBreak(_ delta: Double) {
+		if debug, let cg = ctx?.cgContext {
+			let infraction = y + delta <= bounds.maxY ? y + delta : bounds.maxY
+			cg.saveGState()
+			cg.setStrokeColor(UIColor.red.cgColor)
+			cg.setLineWidth(2.0)
+			let dashes: [CGFloat] = [2, 2]
+			cg.setLineDash(phase: 0, lengths: dashes)
+			let start = CGPoint(x: margin, y: infraction)
+			let end = CGPoint(x: margin + contentWidth, y: infraction)
+			cg.move(to: start)
+			cg.addLine(to: end)
+			cg.strokePath()
+			cg.restoreGState()
+		}
+	}
+
+	private func debugDrawFrames() {
+		if debug, let cg = ctx?.cgContext {
+			cg.saveGState()
+			// Stroke full bounds in red
+			cg.setStrokeColor(UIColor.red.cgColor)
+			cg.setLineWidth(1)
+			cg.stroke(bounds)
+			// Stroke content margins in blue
+			cg.setStrokeColor(UIColor.blue.cgColor)
+			cg.stroke(contentRect)
+			cg.restoreGState()
+		}
+	}
+
 	public func consume(_ delta: Double) {
 		y += delta
+		if debug {
+			debugDrawConsumption()
+		}
+	}
+
+	private func debugDrawConsumption() {
+		if debug, let cg = ctx?.cgContext {
+			cg.saveGState()
+			cg.setStrokeColor(UIColor.gray.cgColor)
+			cg.setLineWidth(0.5)
+			let dashes: [CGFloat] = [2, 2]
+			cg.setLineDash(phase: 0, lengths: dashes)
+			let start = CGPoint(x: margin, y: y)
+			let end = CGPoint(x: margin + contentWidth, y: y)
+			cg.move(to: start)
+			cg.addLine(to: end)
+			cg.strokePath()
+			cg.restoreGState()
+		}
 	}
 }
+
