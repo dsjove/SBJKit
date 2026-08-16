03160e45da9333d54077f75e28fda57660d272fe
David Giovannini <dsjove@mac.com>
2026-08-15 21:38:27 -0500

    shared code

--- Sources/SBJKit/string/String+.swift
+++ Sources/SBJKit/string/String+.swift
@@ -37,3 +37,24 @@ public extension Collection where Element: Hashable {
 		Dictionary(grouping: self, by: keySelector)
 	}
 }
+
+public extension Int {
+	func signedDescription(apply: Bool = true, zero: Bool = true) -> String {
+		if apply && ((zero && self == 0) || (self > 0)) {
+			"+\(self)"
+		} else {
+			"\(self)"
+		}
+	}
+
+	var modifierDescription: String {
+		switch self {
+		case let value where value > 0:
+			"+ \(value)"
+		case let value where value < 0:
+			"- \(abs(value))"
+		default:
+			""
+		}
+	}
+}
